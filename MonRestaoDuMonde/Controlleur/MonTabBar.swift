fea30de324cad318e2922ac643fb7bba70af7cad
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-01 11:47:28 +0200

    tabBar custom

---
+++ MonRestaoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,22 @@
+//
+//  MonTabBar.swift
+//  MonRestaoDuMonde
+//
+//  Created by Snoopy on 01/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+    }
+
+}
---
+++ MonRestaoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  MonRestaoDuMonde
+//
+//  Created by Snoopy on 01/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+
