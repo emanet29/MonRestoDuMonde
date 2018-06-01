7d71e5f65d75ebd4de49501fe590def44d5a140f
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-01 11:34:31 +0200

    ajout des controlleurs principaux

---
+++ MonRestaoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuController.swift
+//  MonRestaoDuMonde
+//
+//  Created by Snoopy on 01/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
