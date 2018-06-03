594653a19329093a288231b12d1632e72f7d1b93
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-03 14:43:41 +0200

    Ajout CollectionView

---
+++ MonRestaoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCell.swift
+//  MonRestaoDuMonde
+//
+//  Created by Snoopy on 03/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+    
+    
+}
