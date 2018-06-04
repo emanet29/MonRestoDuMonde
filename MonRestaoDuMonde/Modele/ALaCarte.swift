6c092563f4e1a3f1af454218dc9d36e3867ffeec
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-04 10:57:27 +0200

    ALaCarte section

---
+++ MonRestaoDuMonde/Modele/ALaCarte.swift
@@ -0,0 +1,27 @@
+//
+//  ALaCarte.swift
+//  MonRestaoDuMonde
+//
+//  Created by Snoopy on 04/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class ALaCarte {
+    private var _type: Type
+    private var _plats: [Plat]
+    
+    var type:Type {
+        return _type
+    }
+    
+    var plats: [Plat]{
+        return _plats
+    }
+    
+    init(type: Type) {
+        self._type = type
+        self._plats = [Plat]()
+    }
+}
